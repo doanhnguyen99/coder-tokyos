338487e21c7b55c87561fd0dd05a31ced0bd1df7
Doanh Nguyen <doanh080199@gmail.com>
2019-07-28 00:29:06 +0700

    Practice github

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var Dog = require('./Dog');
 var Cat = require('./Cat');
 var mic = new Dog('Tom');
 
-var jerry = new Cat();
+// var jerry = new Cat();
 
 
-//mic.sayHi();
-mic.eat(jerry);
-console.log(mic);
+// //mic.sayHi();
+// mic.eat(jerry);
+// console.log(mic);
 
-mic.prototype.getData = function()
-{
-    this.name = 'ahihi';
-}
+// mic.prototype.getData = function()
+// {
+//     this.name = 'ahihi';
+// }
 
-// Comment Code
+// // Comment Code
